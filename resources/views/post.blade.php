99322c8e23331abcdbe849728237736c9c170b22
Andrey <kynterets@gmail.com>
2021-04-11 13:17:11 +0400

    Learning Laravel

---
+++ resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<!doctype html>
+
+<title>My blog</title>
+<link rel="stylesheet" href="app.css">
+
+<body>
+    <article>
+        <?= $post; ?>
+    </article>
+
+    <a href="/">Назад</a>
+</body>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('posts');
+});
+
+Route::get('posts/{post}', function ($slug) {
+
+    if (!file_exists($path = __DIR__ . "/../resources/posts/{$slug}")) {
+        return redirect('/');
+        //abort(404);
+    }
+
+    $post = cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
+
+    return view('post', ['post' => $post]);
+})->where('post', '[A-z.]+');
