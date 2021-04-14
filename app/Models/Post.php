68c114698d7e6f18ae3d1f3eef4efa42f4ed07ba
Andrey <kynterets@gmail.com>
2021-04-14 14:21:17 +0400

    edited posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,21 +4,43 @@ namespace App\Models;
 
 use Illuminate\Support\Facades\File;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 class Post
 {
+    public $title;
+    public $excerpt;
+    public $date;
+    public $body;
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
     public static function all()
     {
-        $files = File::AllFiles(resource_path('posts'));
-        return array_map(fn ($file) => $file->getContents(), $files);
+        return cache()->rememberForever('posts.all', function () {
+            return collect(File::files(resource_path('posts')))
+                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
+                ->map(fn ($document) => new Post(
+                    $document->title,
+                    $document->excerpt,
+                    $document->date,
+                    $document->body(),
+                    $document->slug
+                ))
+                ->sortByDesc('date');
+        });
     }
 
     public static function find($slug)
     {
-        if (!file_exists($path = resource_path("posts/{$slug}"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{$slug}", 1200, fn () => file_get_contents($path));
+        return static::all()->firstWhere('slug', $slug);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,10 +4,16 @@
 <link rel="stylesheet" href="app.css">
 
 <body>
-    <?php foreach ($posts as $post) : ?>
-        <article>
-            <?= $post ?>
-        </article>
-    <?php endforeach; ?>
+    @foreach ($posts as $post)
+    <article class="{{$loop->even ? "mb-6" : ''}}">
+        <h1>
+            <a href="posts/{{ $post->slug}}">
+                {{$post->title}}
+            </a>
+        </h1>
+
+        <div>{!!$post->body!!}</div>
+    </article>
+    @endforeach
 
 </body>
